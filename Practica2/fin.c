7fd81dc75ca779338865c3b080526153bb90de0c
lilium <lilium@becarios.tonejito.info>
2019-02-07 09:57:36 -0600

    Comentarios agregados a fin.c

--- Practica2/fin.c
+++ Practica2/fin.c
@@ -1,24 +1,28 @@
 #include <limits.h>
 #include <stdio.h>
 #include <time.h>
-
-#define CST (-6)
+/*
+ * Programa que calcula la fecha del problema 2k38y
+ * en la fecha exacta en la ciudad de Mexico
+ * 
+ * Autor: Diana Tadeo
+ */
+#define CST (-6)//Central Standard time
 
 int main(){
-	
-	time_t current_date = time(NULL);
+	time_t actual = time(NULL);
 	time_t rawtime;
 	struct tm *info;
 	time(&rawtime);
 	info = gmtime(&rawtime );
-	printf("Fecha Ciudad de Mexico : %2d/%2d/%4d %02d:%02d:%02d",info->tm_mday,info->tm_mon,1900+info->tm_year,(info->tm_hour+CST)%24, info->tm_min,info->tm_sec);
-	printf("\n%s\n", asctime(localtime(&current_date)));
+	//printf("Fecha Ciudad de Mexico :\n %2d/%2d/%4d %02d:%02d:%02d",info->tm_mday,info->tm_mon,1900+info->tm_year,(info->tm_hour+CST)%24, info->tm_min,info->tm_sec);
+	printf("Fecha actual:\n %s\n", asctime(localtime(&actual)));
 	
-	time_t last_day = (time_t)INT_MAX;
-	info = gmtime(&last_day );
+	time_t final = (time_t)INT_MAX;
+	info = gmtime(&final );
 		
-	printf("\nFecha en que acaba el espacio para Unix en CDMX: \n");
-	printf("%2d/%2d/%4d %02d:%02d:%02d",info->tm_mday,info->tm_mon,1900+info->tm_year,(info->tm_hour+CST)%24, info->tm_min,info->tm_sec);
-	printf("\n%s\n", asctime(localtime(&last_day)));
+	printf("Fecha en que acaba el espacio para Unix en CDMX: \n");
+	//printf("%2d/%2d/%4d %02d:%02d:%02d",info->tm_mday,info->tm_mon,1900+info->tm_year,(info->tm_hour+CST)%24, info->tm_min,info->tm_sec);
+	printf("%s\n", asctime(localtime(&final)));
 	return 0;
 }
